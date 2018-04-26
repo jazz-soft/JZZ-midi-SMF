ff257389a94e77fae6434828109890321f1473a6
Sema <sema@pyva.net>
2018-04-25 23:53:28 -0400

    Added Player

--- javascript/JZZ.midi.SMF.js
+++ javascript/JZZ.midi.SMF.js
@@ -12,6 +12,8 @@
 
   if (JZZ.MIDI.SMF) return;
 
+  function _error(s) { throw new Error(s); }
+
   function SMF() {
     var type=1;
     var ppqn=96;
@@ -35,16 +37,16 @@
       fps = parseInt(arguments[1]);
       ppf = parseInt(arguments[2]);
     }
-    else if (arguments.length) throw new Error('Invalid parameters');
-    if (isNaN(type) || type < 0 || type > 2) throw new Error('Invalid parameters');
+    else if (arguments.length) _error('Invalid parameters');
+    if (isNaN(type) || type < 0 || type > 2) _error('Invalid parameters');
     this.type = type;
     if (fps == undefined) {
-      if (isNaN(ppqn) || ppqn < 0 || type > 0xffff) throw new Error('Invalid parameters');
+      if (isNaN(ppqn) || ppqn < 0 || type > 0xffff) _error('Invalid parameters');
       this.ppqn = ppqn;
     }
     else {
-      if (fps != 24 && fps != 25 && fps != 29 && fps != 30) throw new Error('Invalid parameters');
-      if (isNaN(ppf) || ppf < 0 || type > 0xff) throw new Error('Invalid parameters');
+      if (fps != 24 && fps != 25 && fps != 29 && fps != 30) _error('Invalid parameters');
+      if (isNaN(ppf) || ppf < 0 || type > 0xff) _error('Invalid parameters');
       this.fps = fps;
       this.ppf = ppf;
     }
@@ -54,7 +56,7 @@
   SMF.prototype.constructor = SMF;
 
   SMF.prototype.dup = function(x) {
-    throw new Error('Copy-constructor not yet implemented');
+    _error('Copy-constructor not yet implemented');
   };
 
   SMF.prototype.load = function(s) {
@@ -65,7 +67,7 @@
   };
 
   SMF.prototype.loadSMF = function(s) {
-    if (s.substr(0, 8) != 'MThd\0\0\0\6') throw new Error('Not a MIDI file');
+    if (s.substr(0, 8) != 'MThd\0\0\0\6') _error('Not a MIDI file');
     this.type = s.charCodeAt(8) * 16 + s.charCodeAt(9);
     this.ntrk = s.charCodeAt(10) * 16 + s.charCodeAt(11);
     if (s.charCodeAt(12) > 0x7f) {
@@ -75,7 +77,7 @@
     else{
       this.ppqn = s.charCodeAt(12) * 256 + s.charCodeAt(13);
     }
-    if (this.type > 2 || (this.type == 0 && this.ntrk > 1) || (!this.ppf && !this.ppqn)) throw new Error('Invalid MIDI header');
+    if (this.type > 2 || (this.type == 0 && this.ntrk > 1) || (!this.ppf && !this.ppqn)) _error('Invalid MIDI header');
     var n = 0;
     var p = 14;
     while (p < s.length) {
@@ -87,7 +89,7 @@
       this.push(new Chunk(type, data));
       p += len;
     }
-    if (p != s.length || n != this.ntrk) throw new Error("Corrupted MIDI file");
+    if (p != s.length || n != this.ntrk) _error("Corrupted MIDI file");
   };
 
   SMF.prototype.dump = function() {
@@ -114,51 +116,54 @@
     x += s.charCodeAt(2) & 0x7f;
     x <<= 7;
     if (s.charCodeAt(3) < 0x80) return x + s.charCodeAt(3);
-    throw new Error("Corrupted MIDI track");
+    _error("Corrupted MIDI track");
   }
   function _msglen(n) {
     switch (n & 0xf0) {
       case 0x80: case 0x90: case 0xa0: case 0xb0: case 0xe0: return 2;
       case 0xc0: case 0xD0: return 1;
-      default: throw new Error("Corrupted MIDI track");
+      default: _error("Corrupted MIDI track");
     }
   }
 
-//JZZ_.MidiFile.prototype.player=function(){
-// var pl=new JZZ_.MidiFile.Player;
-// pl.ppqn=this.ppqn; pl.fps=this.fps; pl.ppf=this.ppf;
-// var i;
-// var tt=[];
-// for(i=0;i<this.length;i++) if(this[i] instanceof JZZ_.MidiFile.MTrk) tt.push(this[i]);
-// pl.tracks=tt.length;
-// if(this.type==2) throw new Error("Playing MIDI file type 2 not yet implemented");
-// var pp=[]; for(i=0;i<tt.length;i++) pp[i]=0;
-// var t=0;
-// while(true){
-//  var b=true;
-//  var m;
-//  for(i=0;i<tt.length;i++){
-//   while(pp[i]<tt[i].length && tt[i][pp[i]].time==t){
-//    var obj=tt[i][pp[i]];
-//    var evt=new JZZ_.MidiFile.Event;
-//    for(var attr in obj) if(obj.hasOwnProperty(attr)) evt[attr]=obj[attr];
-//    evt.track=i;
-//    pl.push(evt); pp[i]++;
-//   }
-//   if(pp[i]>=tt[i].length) continue;
-//   if(b) m=tt[i][pp[i]].time;
-//   b=false;
-//   if(m>tt[i][pp[i]].time) m=tt[i][pp[i]].time;
-//  }
-//  t=m;
-//  if(b) break;
-// }
-// return pl;
-//}
+  SMF.prototype.player = function() {
+    var pl = new Player();
+    pl.ppqn = this.ppqn;
+    pl.fps = this.fps;
+    pl.ppf = this.ppf;
+    var i;
+    var tt = [];
+    for (i = 0; i < this.length; i++) if (this[i] instanceof MTrk) tt.push(this[i]);
+    if (this.type == 2) _error('Playing MIDI file type 2 not yet implemented');
+    var pp = [];
+    for (i = 0; i < tt.length; i++) pp[i] = 0;
+    var t = 0;
+    while (true) {
+      var b = true;
+      var m;
+      for (i = 0; i < tt.length; i++) {
+        while (pp[i] < tt[i].length && tt[i][pp[i]].time == t) {
+          var obj = tt[i][pp[i]];
+          var evt = new Event();
+          for (var attr in obj) if (obj.hasOwnProperty(attr)) evt[attr] = obj[attr];
+          evt.track = i;
+          pl.push(evt);
+          pp[i]++;
+        }
+        if (pp[i] >= tt[i].length) continue;
+        if(b) m = tt[i][pp[i]].time;
+        b = false;
+        if(m > tt[i][pp[i]].time) m = tt[i][pp[i]].time;
+      }
+      t = m;
+      if (b) break;
+    }
+    return pl;
+  };
 
   function Chunk(t, d) {
     if (this.sub[t]) return this.sub[t](t, d);
-    if (typeof t != 'string' || t.length != 4) throw new Error("Invalid chunk type: " + t);
+    if (typeof t != 'string' || t.length != 4) _error("Invalid chunk type: " + t);
     this.type = t;
     this.data = d;
   }
@@ -166,7 +171,7 @@
   Chunk.prototype.constructor = Chunk;
 
   Chunk.prototype.sub = {
-    'MThd': function() { throw new Error("Illegal chunk type: MThd"); },
+    'MThd': function() { _error("Illegal chunk type: MThd"); },
     'MTrk': function(t, d) { return new MTrk(d); }
   };
   Chunk.prototype.dump = function() {
@@ -183,28 +188,31 @@
     var t = 0;
     var p = 0;
     var w = '';
+    var d;
+    var st;
+    var m;
     while (p < s.length) {
-      var d = _var2num(s.substr(p, 4));
+      d = _var2num(s.substr(p, 4));
       p++;
       if (d > 0x7f) p++;
       if (d > 0x3fff) p++;
       if (d > 0x1fffff) p++;
       t += d;
       if (s.charCodeAt(p) == 0xff) {
-        var st = s.substr(p, 2);
+        st = s.substr(p, 2);
         p += 2;
-        var m = _var2num(s.substr(p, 4));
+        m = _var2num(s.substr(p, 4));
         p++;
         if (m > 0x7f) p++;
         if (m > 0x3fff) p++;
         if (m > 0x1fffff) p++;
         this.push (new Event(t, st, s.substr(p, m)));
-        p+=m;
+        p += m;
       }
       else if (s.charCodeAt(p) == 0xf0 || s.charCodeAt(p) == 0xf7) {
-        var st = s.substr(p, 1);
-        p+=1;
-        var m = _var2num(s.substr(p, 4));
+        st = s.substr(p, 1);
+        p += 1;
+        m = _var2num(s.substr(p, 4));
         p++;
         if (m > 0x7f) p++;
         if (m > 0x3fff) p++;
@@ -215,12 +223,12 @@
       else if (s.charCodeAt(p) & 0x80) {
         w = s.substr(p, 1);
         p += 1;
-        var m = _msglen(w.charCodeAt(0));
+        m = _msglen(w.charCodeAt(0));
         this.push(new Event(t, w, s.substr(p, m)));
         p += m;
       }
       else if (w.charCodeAt(0) & 0x80) {
-        var m = _msglen(w.charCodeAt(0));
+        m = _msglen(w.charCodeAt(0));
         this.push(new Event(t, w, s.substr(p, m)));
         p += m;
       }
@@ -233,8 +241,10 @@
     var s = '';
     var t = 0;
     var m = '';
+    var d;
+    var len;
     for (var i = 0; i < this.length; i++) {
-      var d = this[i].time - t;
+      d = this[i].time - t;
       t = this[i].time;
       if (d > 0x1fffff) s += String.fromCharCode(((d >> 21) & 0x7f) + 0x80);
       if (d > 0x3fff) s += String.fromCharCode(((d >> 14) & 0x7f) + 0x80);
@@ -242,7 +252,7 @@
       s += String.fromCharCode(d & 0x7f);
       if (this[i].status.charCodeAt(0) == 0xff || this[i].status.charCodeAt(0) == 0xf0 || this[i].status.charCodeAt(0) == 0xf7) {
         s += this[i].status;
-        var len = this[i].data.length;
+        len = this[i].data.length;
         if (len > 0x1fffff) s += String.fromCharCode(((len >> 21) & 0x7f) + 0x80);
         if (len > 0x3fff) s += String.fromCharCode(((len >> 14) & 0x7f) + 0x80);
         if (len > 0x7f) s += String.fromCharCode(((len >> 7) & 0x7f) + 0x80);
@@ -254,16 +264,16 @@
       }
       s += this[i].data;
     }
-    var len = s.length;
+    len = s.length;
     return 'MTrk' + String.fromCharCode((len >> 24) & 255) + String.fromCharCode((len >> 16) & 255) + String.fromCharCode((len >> 8) & 255) + String.fromCharCode(len & 255) + s;
   };
   MTrk.prototype.getTime = function() { return this[this.length - 1].time; };
   MTrk.prototype.setTime = function(t) {
     t = parseInt(t);
-    if (isNaN(t) || t < 0) throw new Error('Invalid parameter');
+    if (isNaN(t) || t < 0) _error('Invalid parameter');
     var e = this[this.length - 1];
     if (t < e.time) {
-      throw new Error("not yet implemented");
+      _error("not yet implemented");
     }
     e.time = t;
   };
@@ -290,10 +300,10 @@
     if (x != undefined) return x;
     if ((s.charCodeAt(0) >> 4) == 9) return d.charCodeAt(1) ? 14 : 10;
     return 18;
-  };
+  }
   MTrk.prototype.addEvent = function(t, s, d) {
     t = parseInt(t);
-    if(isNaN(t) || t < 0) throw new Error('Invalid parameter');
+    if(isNaN(t) || t < 0) _error('Invalid parameter');
     s = s.toString();
     d = d.toString();
     var i;
@@ -312,24 +322,24 @@
 //!!!!!!!
     var msg = JZZ_.MIDI.apply(undefined, args).data();
     var x = msg.charCodeAt(0);
-    if (x < 0x80 || x > 0xfe) throw new Error('Invalid MIDI message');
+    if (x < 0x80 || x > 0xfe) _error('Invalid MIDI message');
 //!!!!!!!
     var y = JZZ_.Midi.len(x);
-    if (y != undefined && y != msg.length) throw new Error('Invalid MIDI message');
+    if (y != undefined && y != msg.length) _error('Invalid MIDI message');
     this.addEvent(t, msg.substr(0, 1), msg.substr(1));
   };
   MTrk.prototype.addNote = function(t, ch, note, vel, dur) {
     var n = parseInt(note);
     if (isNaN(n)) n = parseInt(JZZ_.Midi[note]);
-    if (isNaN(n) || n < 0 || n > 127) throw new Error('Invalid parameter');
+    if (isNaN(n) || n < 0 || n > 127) _error('Invalid parameter');
     ch = parseInt(ch);
-    if (isNaN(ch) || ch < 0 || ch > 15) throw new Error('Invalid parameter');
+    if (isNaN(ch) || ch < 0 || ch > 15) _error('Invalid parameter');
     if (dur == undefined) dur = 0;
     dur = parseInt(dur);
-    if (isNaN(dur) || dur < 0) throw new Error('Invalid parameter');
+    if (isNaN(dur) || dur < 0) _error('Invalid parameter');
     if (vel == undefined) vel = 127;
     vel = parseInt(vel);
-    if (isNaN(vel) || vel < 0 || vel > 127) throw new Error('Invalid parameter');
+    if (isNaN(vel) || vel < 0 || vel > 127) _error('Invalid parameter');
     this.addMidi(t, 0x90 + ch, n, vel);
     if(dur) this.addMidi(t + dur, 0x90 + ch, n, 0);
   }; // need to rewrite!
@@ -349,6 +359,7 @@
   }
   Event.prototype.toString = function() {
     var str;
+    var i;
     var s0 = this.status.charCodeAt(0);
     if (s0 > 0x7f && s0 != 0xff && s0 != 0xf0 && s0 != 0xf7) return JZZ_.MIDI(this.status + this.data).toString();
     if (s0 == 0xff) {
@@ -368,7 +379,7 @@
           6: 'Marker',
           7: 'Cue point',
           8: 'Program name',
-          9:'Device name'
+          9: 'Device name'
         }[s1];
         str += ': ' + this.data;
         return str;
@@ -387,7 +398,7 @@
       }
       else if (s1 == 0x54) {
         str += 'SMPTE offset: ' + (this.data.charCodeAt(0) > 9 ? '' : '0') + this.data.charCodeAt(0).toString();
-        for (var i = 1; i < this.data.length; i++) str += ':' + (this.data.charCodeAt(i) > 9 ? '' : '0') + this.data.charCodeAt(i);
+        for (i = 1; i < this.data.length; i++) str += ':' + (this.data.charCodeAt(i) > 9 ? '' : '0') + this.data.charCodeAt(i);
         return str;
       }
       else if (s1 == 0x58) {
@@ -414,54 +425,88 @@
     else if (s0 == 0xf0) str = 'SysEx: f0';
     else if (s0 == 0xf7) str = 'SysEx cont.:';
     else str = (s0 > 15 ? '' : '0') + s0.toString(16);
-    for (var i = 0; i < this.data.length; i++) str += ' ' + (this.data.charCodeAt(i) > 15 ? '' : '0') + this.data.charCodeAt(i).toString(16);
+    for (i = 0; i < this.data.length; i++) str += ' ' + (this.data.charCodeAt(i) > 15 ? '' : '0') + this.data.charCodeAt(i).toString(16);
     return str;
   };
 
+  function Player() {
+    this.playing = false;
+    this.looped = false;
+    this._data = [];
+  }
+  Player.prototype = new JZZ.Widget();
+  Player.prototype.constructor = Player;
 
-  _b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-  function _fromBase64(input) {
-    var output = '';
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    input=input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-    while (i < input.length) {
-      enc1 = _b64.indexOf(input.charAt(i++));
-      enc2 = _b64.indexOf(input.charAt(i++));
-      enc3 = _b64.indexOf(input.charAt(i++));
-      enc4 = _b64.indexOf(input.charAt(i++));
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-      output = output + String.fromCharCode(chr1);
-      if (enc3 != 64) {
-        output = output + String.fromCharCode(chr2);
+  Player.prototype.loop = function(b) { this.looped = b ? true : false; };
+  Player.prototype.play = function() {
+    this.onEvent({ control: 'play' });
+    if (this.ppqn) this.mul = this.ppqn / 500.0;
+    else this.mul = this.fps * this.ppf / 1000.0;
+    this.playing = true;
+    this.ptr = 0;
+    this.c0 = 0;
+    this.t0 = JZZ.now();
+    this.tick();
+  };
+  Player.prototype.stop = function() {
+    this.event = 'stop';
+    this.paused = undefined;
+    if (this.playing) this.playing = false;
+    else this.onEvent({ control: this.event });
+  };
+  Player.prototype.pause = function() {
+    this.event = 'pause';
+    this.playing = false;
+  };
+  Player.prototype.resume = function() {
+    if (this.playing || this.paused == undefined) return;
+    var t = JZZ.now();
+    this.t0 += JZZ.now() - this.paused;
+    this.onEvent({ control: 'resume' });
+    this.playing = true;
+    this.tick();
+  };
+  Player.prototype.tick = function() {
+    var t = JZZ.now();
+    var c = this.c0 + (t - this.t0) * this.mul;
+    var e;
+    var evt;
+    for(; this.ptr < this.length; this.ptr++) {
+      e = this._data[this.ptr];
+      if (e.time > c) break;
+      evt = {};
+      if (e.status == '\xff\x51' && this.ppqn) {
+        this.mul = this.ppqn * 1000.0 / ((e.data.charCodeAt(0) << 16) + (e.data.charCodeAt(1) << 8) + e.data.charCodeAt(2));
+        this.t0 = t;
+        this.c0 = c;
       }
-      if (enc4 != 64) {
-        output = output + String.fromCharCode(chr3);
+      else if (e.status.charCodeAt(0) == 0xf7) { evt.midi = JZZ_.MIDI(e.data); }
+      else if (e.status.charCodeAt(0) != 0xff) { evt.midi = JZZ_.MIDI(e.status + e.data); }
+      else {
+        evt.status = e.status;
+        evt.data=e.data;
       }
+      evt.track = e.track;
+      if (typeof e.user != 'undefined') evt.user = e.user;
+      this.onEvent(evt);
     }
-    return output;
-  }
-  function _toBase64(data) {
-    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0, ac = 0, enc = '', tmp_arr = [];
-    if (!data) return data;
-    do {
-      o1 = data.charCodeAt(i++);
-      o2 = data.charCodeAt(i++);
-      o3 = data.charCodeAt(i++);
-      bits = o1 << 16 | o2 << 8 | o3;
-      h1 = bits >> 18 & 0x3f;
-      h2 = bits >> 12 & 0x3f;
-      h3 = bits >> 6 & 0x3f;
-      h4 = bits & 0x3f;
-      tmp_arr[ac++] = _b64.charAt(h1) + _b64.charAt(h2) + _b64.charAt(h3) + _b64.charAt(h4);
-    } while (i < data.length);
-    enc = tmp_arr.join('');
-    var r = data.length % 3;
-    return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
-  }
+    if (this.ptr >= this.length) {
+      this.onEvent({ control: 'end' });
+      if (this.looped) {
+        this.ptr = 0;
+        this.c0 = 0;
+        this.t0 = t;
+      }
+      else this.stop();
+    }
+    var f = (function(x) { return function(){ x.tick(); }; })(this);
+    if (this.playing) {
+      window.setTimeout(f, 0);
+      return;
+    }
+    if (this.event == 'pause') this.paused = t;
+    this.onEvent({ control: this.event });
+  };
 
   JZZ.MIDI.SMF = SMF;
 });
